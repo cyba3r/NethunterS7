ee039384da8fa50e1df687cbbef1b3f43738df65
cyba3r <cyba3r@gmail.com>
2019-05-22 06:04:13 +0200

    Init

---
+++ Nethunter-s7-kernel-StockOreo/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#1 SMP PREEMPT Wed May 22 01:20:40 CEST 2019"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "kali"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
---
+++ Nethunter-s7-kernel-StockOreo/include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.18.91.Nethunter-WirusMOD-v1.4+"
